27419f9f8f416adbe6d549c3202c306eac7f0e44
je-collins <45102535+je-collins@users.noreply.github.com>
2021-01-10 19:28:13 -0500

    Add comment

--- Ethics/src/com/jordan/ethics/College.java
+++ Ethics/src/com/jordan/ethics/College.java
@@ -1,5 +1,9 @@
 package com.jordan.ethics;
 
+/**
+ * Enum to represent the different colleges.
+ * @author jecol
+ */
 public enum College {
 	BA, BHC, CAH, COM, CON, COS, ECS, ED, HPA, OPT, RCHM, UGST
 }
